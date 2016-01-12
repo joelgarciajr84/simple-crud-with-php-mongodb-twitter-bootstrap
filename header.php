c47065c1a817466b8226378cc0267b40b4c9e251
Joel Garcia Jr <joel.garciajr84@gmail.com>
2016-01-12 09:04:34 -0200

    Dinamic active class on tabs menu

--- header.php
+++ header.php
@@ -1,14 +1,27 @@
-<?php 
+<?php
 include "header.html";
 require_once("connection.php");
+
+$MenuLabelAndActions = array(
+    'Create' => 'index.php'
+    ,'Update' => 'update.php'
+    , 'Delete' => 'delete.php'
+    );
+$Currenturl = 'http://'.$_SERVER[HTTP_HOST].$_SERVER[REQUEST_URI];
+$UrlAction = end((explode('/', $Currenturl)));
+
+if(empty($UrlAction)){
+    $UrlAction = 'index.php';
+}
 ?>
   <body>
 	 <div align="center" class="well">
         <h2>Simple MongoDB CRUD using PHP</h2>
 
 		<ul class="nav nav-pills">
-			<li class="active"><a href="#">Create</a></li>
-			<li><a href="update.php">Update</a></li>
-			<li><a href="delete.php">Delete</a></li>
+        <?php foreach ($MenuLabelAndActions as $Label => $Action):?>
+
+			<li <?php if($Action == $UrlAction){ echo "class='active'";}?>><a href="<?php echo $Action; ?>"><?php echo $Label; ?></a></li>
+			<?php endforeach; ?>
 		</ul>
 	</div>
